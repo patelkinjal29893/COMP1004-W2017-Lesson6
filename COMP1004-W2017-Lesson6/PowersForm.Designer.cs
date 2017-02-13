1b6035c0b83142e5d8f03252c3246b8edc680b76
KinjalPatel <patelkinjal298@hotmail.com>
2017-02-13 17:34:32 -0500

    Updated PowersForm

--- COMP1004-W2017-Lesson6/PowersForm.Designer.cs
+++ COMP1004-W2017-Lesson6/PowersForm.Designer.cs
@@ -28,11 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PowerListLabel = new System.Windows.Forms.Label();
+            this.PowerListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // PowerListLabel
+            // 
+            this.PowerListLabel.AutoSize = true;
+            this.PowerListLabel.Location = new System.Drawing.Point(21, 24);
+            this.PowerListLabel.Name = "PowerListLabel";
+            this.PowerListLabel.Size = new System.Drawing.Size(56, 13);
+            this.PowerListLabel.TabIndex = 0;
+            this.PowerListLabel.Text = "Power List";
+            // 
+            // PowerListBox
+            // 
+            this.PowerListBox.FormattingEnabled = true;
+            this.PowerListBox.Items.AddRange(new object[] {
+            "Telepathy",
+            "Body Armour",
+            "Fight",
+            "Telekinesis",
+            "Electrical Bolt",
+            "Fire Ball",
+            "Combat Senses",
+            "Radiation Resistance"});
+            this.PowerListBox.Location = new System.Drawing.Point(24, 40);
+            this.PowerListBox.MultiColumn = true;
+            this.PowerListBox.Name = "PowerListBox";
+            this.PowerListBox.Size = new System.Drawing.Size(120, 121);
+            this.PowerListBox.TabIndex = 1;
+            // 
+            // PowersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.PowerListBox);
+            this.Controls.Add(this.PowerListLabel);
+            this.Name = "PowersForm";
             this.Text = "PowersForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label PowerListLabel;
+        private System.Windows.Forms.ListBox PowerListBox;
     }
 }
